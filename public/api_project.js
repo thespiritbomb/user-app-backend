039ae71f3a0612fbfeab844a97a1db2b416db65c
Niyati Rana <47718791+thespiritbomb@users.noreply.github.com>
2020-04-14 22:13:00 +0530

    minor changes v3

--- public/api_project.js
+++ public/api_project.js
@@ -3,8 +3,8 @@ define({
   "version": "1.0.0",
   "description": "API for adding and listing user to mongoDB databse",
   "title": "User Form Database API",
-  "url": "https://algo-user-backend.herokuapp.com/",
-  "sampleUrl": "https://algo-user-backend.herokuapp.com/",
+  "url": "https://algo-user-backend.herokuapp.com",
+  "sampleUrl": "https://algo-user-backend.herokuapp.com",
   "template": {
     "forceLanguage": "en"
   },
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-14T16:39:44.788Z",
+    "time": "2020-04-14T16:42:34.806Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
